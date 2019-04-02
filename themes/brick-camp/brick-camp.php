a680cc04036ec2a727a5e45d73df0a9a87698775
brickup <info@brickup.de>
2019-04-02 22:36:14 +0200

    Added angle-size table to "pythagoras twist"

--- themes/brick-camp/brick-camp.php
+++ themes/brick-camp/brick-camp.php
@@ -5,6 +5,7 @@ use Grav\Common\Grav;
 use Grav\Common\Theme;
 use Grav\Common\Page\Pages;
 use Grav\Common\Twig\Twig;
+use RocketTheme\Toolbox\Event\Event;
 
 class BrickCamp extends Theme
 {
@@ -21,5 +22,25 @@ class BrickCamp extends Theme
             $this->active = false;
             return;
         }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onPageContentProcessed' => ['onPageContentProcessed', 0],
+        ]);
+    }
+      
+    /**
+     * Add bootstrap classes to markdown results
+     * @param Event $event
+     * @return void
+     */
+    public function onPageContentProcessed(Event $event) 
+    {
+        $page = $event['page'];
+        $raw_content = $page->content();
+        $raw_content = preg_replace('/<table>/', '<div class="table-responsive"><table class="table table-sm table-striped table-bordered text-center">', $raw_content);
+        $raw_content = preg_replace('/<th>/', '<th scope="col">', $raw_content);
+        $raw_content = preg_replace('/<\/table>/', '</table></div>', $raw_content);
+        $page->setRawContent($raw_content);
     }
 }
